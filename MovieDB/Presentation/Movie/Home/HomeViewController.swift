e48aca02857108339e356887d5eba367e74a208d
yxgg <yogajktf4@gmail.com>
2023-05-06 01:29:13 +0700

    (TabBar) : configure tabBar

---
+++ MovieDB/Presentation/Movie/Home/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  MovieDB
+//
+//  Created by yxgg on 06/05/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
